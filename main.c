02168a7558ea1f76bbb29e28496dc08407c273bf
Hüseyin Karakaya <karakayahuseyin023@gmail.com>
2024-11-30 21:35:44 +0300

    Added airfoil input check.

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include "nacaAirfoilGenerator.h"
+#include "generator.h"
 
 int main() {
     int num_points = 100;
     double M, P, T ;
     int digit;
     printf("NACA 4digit profili girin: ");
-    scanf("%d", &digit);
+
+    if (scanf("%d", &digit) != 1 || digit < 1000 || digit > 9999) {
+        printf("Hatalı giriş! Lütfen 4 haneli bir sayı girin.\n");
+        return 1;
+    }
+
     M = digit / 1000;
     P = (digit / 100) % 10;
     T = digit % 100;
     Airfoil *airfoil = create_airfoil(num_points, M, P, T);
+    
     if (!airfoil) {
         printf("Airfoil oluşturulamadı.\n");
         return 1;
